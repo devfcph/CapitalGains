4cf40bdaf7bd506f8e5f09619d908537d5b0bd7a
Francisco Pérez <devfcph@gmail.com>
2024-03-21 11:37:40 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChallenge_CapitalGains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
