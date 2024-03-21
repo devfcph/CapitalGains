ce74372133a37a7bf21e2b60581df5822aee499c
Francisco Pérez <devfcph@gmail.com>
2024-03-21 12:08:21 -0600

    Added Capital model

---
+++ Entities/Capital.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenge_CapitalGains.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge_CapitalGains.Entities
+{
+    public class Capital
+    {
+        public OperationType Operation { get; set; }
+
+        public Decimal UnitCost {  get; set; }
+        public Int32 Quantity { get; set; }
+    }
+}
